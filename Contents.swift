5d6f92b76bf5fed100adac1f9bab3065b6aebab6
Veronica Serna <hello.cookyourweb@gmail.com>
2022-12-11 23:52:09 +0100

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -91,181 +91,212 @@ var valorCincuentaNumPrimos: Int = 230
         
         
   /*
-MARK: 4.- Crear un enumerado que permita indicar la posición en el campo de un jugador de fútbol, por ejemplo: Portero, Lateral Derecho, Central, Lateral Izquierdo, Mediocentro, Extremo Derecha, Extremo Izquierda, Delantero, etc
-         
-*/
-        
-        
-        
-        enum PlayerPosition {
-            case Portero
-            case LateralDerecho
-            case Central
-            case LateralIzquierdo
-            case Mediocentro
-            case ExtremoDerecho
-            case ExtremoIzquierdo
-            case Delantero
-            case Ninguno
-            
-        }
-        PlayerPosition.Delantero
-        
-        
+ MARK: 4.- Crear un enumerado que permita indicar la posición en el campo de un jugador de fútbol, por ejemplo: Portero, Lateral Derecho, Central, Lateral Izquierdo, Mediocentro, Extremo Derecha, Extremo Izquierda, Delantero, etc
+ 
+ */
+
+
+
+enum PlayerPosition {
+    case Portero
+    case LateralDerecho
+    case Central
+    case LateralIzquierdo
+    case Mediocentro
+    case ExtremoDerecho
+    case ExtremoIzquierdo
+    case Delantero
+    case Ninguno
+    
+}
+PlayerPosition.Delantero
+
+
 /*
-MARK: 5.- Crear una clase, con los atributos necesarios, para representar a los miembros que participan en una selección del mundial y un enumerado que los diferencie por tipo, por ejemplo: Jugador, Seleccionador, Médico, etc.
-*/
-        
-        ///Swift initializer: class with properties with no default values, usinga a initializer to asign value.
-        
-        
-        class Member {
-            
-            // properties with no default values
-            var name : String = ""
-            var age : UInt = 0
-            var type : DifferentType
-            var position:PlayerPosition
-            // assign value using initializer
-            init (name : String = "",
-                  age : UInt = 0,
-                  type :DifferentType,
-                  position: PlayerPosition){
-                self.name = name
-                self.age = age
-                self.type = type
-                self.position = position
-            }
-            // object of Member with custom initializer
-            var member : String { return "\nMembers name  \(name) has  \(age) years. Plays as \(position) \(type)"}
-            
-            // instance of Member
-            var member1 = Member(name: "Militao",
-                                 age: 34,
-                                 type: Member.DifferentType.Player,
-                                 position: PlayerPosition.LateralIzquierdo)
-            
-            // Enum with types of member of a Word Cup Team
-            enum DifferentType {
-                case President
-                case Manager
-                case Player
-                case Doctor
-                case MassageTherapist
-                case PressManager
-            }
-        }
-        // instance of Member
-        var member2 = Member(name: "Benzema",
-                             age: 25,
-                             type: Member.DifferentType.Player,
-                             position: PlayerPosition.Delantero
-        )
-        print(member2.member)
-     
-     class Team {
-    var name: String
+ MARK: 5.- Crear una clase, con los atributos necesarios, para representar a los miembros que participan en una selección del mundial y un enumerado que los diferencie por tipo, por ejemplo: Jugador, Seleccionador, Médico, etc.
+ */
+
+///Swift initializer: class with properties with no default values, usinga a initializer to asign value.
+
+
+class Member {
+    
+    // properties with no default values
+    var name : String = ""
+    var age : UInt = 0
+    var type : DifferentType
+    var position:PlayerPosition
+    // assign value using initializer
+    init (name : String = "",
+          age : UInt = 0,
+          type :DifferentType,
+          position: PlayerPosition){
+        self.name = name
+        self.age = age
+        self.type = type
+        self.position = position
+    }
+    // object of Member with custom initializer
+    var member : String { return "\nMembers name  \(name) has  \(age) years. Plays as \(position) \(type)"}
+    
+   
+    
+    // Enum with types of member of a Word Cup Team
+    enum DifferentType {
+        case President
+        case Manager
+        case Player
+        case Doctor
+        case MassageTherapist
+        case PressManager
+    }
+}
+
+// instance of Member
+var member1 = Member(name: "Militao",
+                     age: 34,
+                     type: Member.DifferentType.Player,
+                     position: PlayerPosition.LateralIzquierdo)
+var member2 = Member(name: "Benzema",
+                     age: 25,
+                     type: Member.DifferentType.Player,
+                     position: PlayerPosition.Delantero
+)
+print(member2.member, member1.member)
+
+//examples for number six Team Senegal
+var member3 = Member(name: "Player 1 Senegal",
+                     age: 34,
+                     type: Member.DifferentType.Player,
+                     position: PlayerPosition.LateralIzquierdo)
+var member4 = Member(name: "Player 2 Senegal",
+                     age: 25,
+                     type: Member.DifferentType.Player,
+                     position: PlayerPosition.Delantero)
+
+
+class Team {
+    var country: Country
     var player: [Member]
 
-  init(name: String,  players: [Member]) {
-      self.name = name
+  init(country: Country,  players: [Member]) {
+      self.country = country
       self.player = players
   }
 }
-        
-        /*
-         MARK: 6.- Crear las clases necesarias, con los atributos mínimos, para representar las selecciones de fútbol del Mundial de fútbol 2022, por ejemplo: Una clase que represente el Mundial, necesitaremos que contenga un listado de Selecciones, cada selección tendrá sus atributos, como nombre, país, jugadores, seleccionador, etc.
-         
-         grupos del mundial 2022
-         Grupo A: Países Bajos, Senegal, Ecuador, Catar (anfitrión)
-         Grupo B: Inglaterra, Estados Unidos, Irán, Gales.
-         Grupo C: Argentina, Polonia, México, Arabia Saudí
-         Grupo D: Francia, Australia, Túnez, Dinamarca.
-         Grupo E: Japón, España, Alemania, Costa Rica.
-         Grupo F: Marruecos, Croacia, Bélgica, Canadá
-         
-         */
-        enum Groups
-        {
-            case GroupA
-            case GroupB
-            case GroupC
-            case GroupD
-            case GroupE
-            case GroupF
-           
-        }
-       enum Country {
-    case PaisesBajos
-    case Senegal
-    case Ecuador
-    case Catar
-    case Inglaterra
-    case EstadosUnidos
-    case Iran
-    case Gales
-    case Argentina
-    case Polonia
-    case Mexico
-    case ArabiaSaudi
-    case Francia
-    case Australia
-    case Tunez
-    case Dinamarca
-    case Japon
-    case España
-    case Tunez
-    case CostaRica
-    case Marruecos
-    case Croacia
-    case Belgica
-    case Canada
 
-}
-        
-        class WorldCupTeam {
-            
-            var group: Groups
-            var country: Country
-            var players: [Member]
-            var result : Int
-            
-            init ( group: Groups, country : Country, players : [Member] = [], result : Int = 0) {
-                self.group = group
-                self.country = country
-                self.players = players
-                self.result = result
-                
-            }
-        }
-            
-        
-        
-        
 /*
- MARK: 7.- Crear una clase para representar los partidos entre selecciones, deberá contener atributos como equipo local, visitante y resultado como mínimo. Generar una lista aleatoria de partidos entre la lista de selecciones anteriores y hacer un print de este estilo por partido:
-Partido: España 3 - 1 Brasil
- */
-      
-      
-      
-      
-/*
-MARK: 8.- Generar de forma aleatoria, dentro de la clase Mundial, un listado de grupos con un máximo de 4 selecciones por grupo, se puede crear una clase nueva Grupo que contenga el nombre del grupo, listado de participantes y listado de partidos. Por ejemplo: Grupo A España, Brasil, Francia, Alemania.
-
-*/
-        
-        
-/*
-         
- MARK: 9.- Para añadir a cada Grupo los puntos de cada selección habrá que contabilizar las victorias con 3 puntos, empates con 1 y derrotas con 0. Añadir una función en la clase Grupo que le pasemos una selección y nos devuelva sus puntos.*/
-        
-        
-/*
-
- MARK: 10.- Generar los partidos del Mundial en cada grupo y calcular las dos primeras selecciones de cada grupo y hacer un print con los clasificados.
-         
+ MARK: 6.- Crear las clases necesarias, con los atributos mínimos, para representar las selecciones de fútbol del Mundial de fútbol 2022, por ejemplo: Una clase que represente el Mundial, necesitaremos que contenga un listado de Selecciones, cada selección tendrá sus atributos, como nombre, país, jugadores, seleccionador, etc.
+ 
+ grupos del mundial 2022
+ Grupo A: Países Bajos, Senegal, Ecuador, Catar (anfitrión)
+ Grupo B: Inglaterra, Estados Unidos, Irán, Gales.
+ Grupo C: Argentina, Polonia, México, Arabia Saudí
+ Grupo D: Francia, Australia, Túnez, Dinamarca.
+ Grupo E: Japón, España, Alemania, Costa Rica.
+ Grupo F: Marruecos, Croacia, Bélgica, Canadá
  
  */
+enum Groups
+{
+    case GroupA
+    case GroupB
+    case GroupC
+    case GroupD
+    case GroupE
+    case GroupF
+   
+}
+enum Country {
+    case PaisesBajos,
+     Senegal,
+     Ecuador,
+     Catar,
+     Inglaterra,
+     EstadosUnidos,
+   Iran,
+    Gales,
+     Argentina,
+    Polonia,
+     Mexico,
+     ArabiaSaudi,
+     Francia,
+     Australia,
+     Tunez,
+     Dinamarca,
+     Japon,
+     España,
+     CostaRica,
+     Marruecos,
+     Croacia,
+     Belgica,
+     Canada
+
+}
+var paisesBajos = Country.PaisesBajos
+var senegal = Country.Senegal
+
+class WorldCupTeam {
+    
+    var group: Groups
+    var country: Country
+    var players: [Member]
+    var result : Int
+    
+    init ( group: Groups, country : Country, players : [Member] = [], result : Int = 0) {
+        self.group = group
+        self.country = country
+        self.players = players
+        self.result = result
+        
     }
 }
+
+class Games {
+  var team1: Team
+  var team2: Team
+  var result: String
+
+  init(team1: Team, team2: Team, result: String) {
+      self.team1 = team1
+      self.team2 = team2
+      self.result = result
+  }
+}
+
+var teams = [
+    Team (country:paisesBajos , players: [member1, member2]),
+    Team( country:senegal, players: [member3, member4])
+]
+
+
+
+/*
+ MARK: 7.- Crear una clase para representar los partidos entre selecciones, deberá contener atributos como equipo local, visitante y resultado como mínimo. Generar una lista aleatoria de partidos entre la lista de selecciones anteriores y hacer un print de este estilo por partido:
+ Partido: España 3 - 1 Brasil
+ 
+ 
+ */
+
+
+
+
+/*
+ MARK: 8.- Generar de forma aleatoria, dentro de la clase Mundial, un listado de grupos con un máximo de 4 selecciones por grupo, se puede crear una clase nueva Grupo que contenga el nombre del grupo, listado de participantes y listado de partidos. Por ejemplo: Grupo A España, Brasil, Francia, Alemania.
+ 
+ */
+
+
+/*
+ 
+ MARK: 9.- Para añadir a cada Grupo los puntos de cada selección habrá que contabilizar las victorias con 3 puntos, empates con 1 y derrotas con 0. Añadir una función en la clase Grupo que le pasemos una selección y nos devuelva sus puntos.*/
+
+
+/*
+ MARK: 10.- Generar los partidos del Mundial en cada grupo y calcular las dos primeras selecciones de cada grupo y hacer un print con los clasificados.
+ 
+ */
+
+
+
+
